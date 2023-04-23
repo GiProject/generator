915738c7bb08d357a81b46ef620b9c1466a7c4b0
Evgeniy Kulinichev <gekin010@ya.ru>
2023-04-23 23:37:22 +0300

    lint fixed

--- src/js/Team.js
+++ src/js/Team.js
@@ -1,31 +1,29 @@
 export default class Team {
-    constructor(chars) {
-        this.chars = chars;
-    }
+  constructor(chars) {
+    this.chars = chars;
+  }
 
-    [Symbol.iterator]() {
-        const { chars } = this;
-        let i = 0;
-        return {
-            next() {
-                if (i >= chars.length) {
-                    return {
-                        value: undefined,
-                        done: true
-                    }
-                } else if (i == chars.length - 1) {
-                    return {
-                        value: chars[i++],
-                        done: true
-                    }
-                } else {
-                    return {
-                        value: chars[i++],
-                        done: false
-                    }
-                }
-
-            }
+  [Symbol.iterator]() {
+    const { chars } = this;
+    let i = 0;
+    return {
+      next() {
+        if (i >= chars.length) {
+          return {
+            value: undefined,
+            done: true,
+          };
+        } if (i === chars.length - 1) {
+          return {
+            value: chars[i++],
+            done: true,
+          };
         }
-    }
+        return {
+          value: chars[i++],
+          done: false,
+        };
+      },
+    };
+  }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
-import Team from "./Team";
-import Character from "./Character";
+import Team from './Team';
+import Character from './Character';
 
 const char1 = new Character('Лучник', 'Bowman', 50, 1, 40, 10);
 const char2 = new Character('Лучник', 'Bowman', 70, 2, 64, 24);
@@ -8,7 +8,7 @@ const char3 = new Character('Лучник', 'Bowman', 80, 3, 84, 35);
 const team = new Team([
   char1,
   char2,
-  char3
+  char3,
 ]);
 const iterator = team[Symbol.iterator]();
 
